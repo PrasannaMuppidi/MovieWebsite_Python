0e1c09ab7c0ce319cf7087f9ac04f45498e28e11
PrasannaMuppidi <pmyrd@mail.umkc.edu>
2017-02-27 22:24:13 -0600

    Changes to the project movie trailer webpage

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,40 +1,51 @@
-import media                                                                                 #importing the python module media into this python file
-import fresh_tomatoes                                                                        #importing the python module fresh_tomatoes into this python file
-toy_story = media.Movie("Toy story",                                                         #Creating an instance toy_story using the python module media and its class Movie
+import media
+import fresh_tomatoes
+
+#Creating an instance toy_story using the python module media and its class Movie
+toy_story = media.Movie("Toy story",                                                         
                         "A story of a boy and his toys that have life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar",                                                               #Creating an instance avatar using the python module media and its class Movie
+#Creating an instance avatar using the python module media and its class Movie
+avatar = media.Movie("Avatar",                                                               
                      "Some weird avatar story",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-eat_pray_love = media.Movie("Eat Pray Love",                                                #Creating an instance eat_pray_love using the python module media and its class Movie
+#Creating an instance eat_pray_love using the python module media and its class Movie
+eat_pray_love = media.Movie("Eat Pray Love",                                                
                             "Story about love and travel",
                             "https://upload.wikimedia.org/wikipedia/en/7/7e/Eat_pray_love_ver2.jpg",
                             "https://www.youtube.com/watch?v=mjay5vgIwt4")
 
-lion = media.Movie("Lion",                                                                  #Creating an instance lion using the python module media and its class Movie
+#Creating an instance lion using the python module media and its class Movie
+lion = media.Movie("Lion",                                                                  
                    "An adopted boy finding his biological parents in India",
                    "https://upload.wikimedia.org/wikipedia/en/d/d5/Lion_2016_Poster.jpg",
                    "https://www.youtube.com/watch?v=-RNI9o06vqo")
 
-the_blind_side = media.Movie("The Blind Side",                                              #Creating an instance the_blind_side using the python module media and its class Movie
+#Creating an instance the_blind_side using the python module media and its class Movie
+the_blind_side = media.Movie("The Blind Side",                                              
                             "Story about a big boy finding a new family",
                             "https://upload.wikimedia.org/wikipedia/en/6/60/Blind_side_poster.jpg",
                             "https://www.youtube.com/watch?v=gvqj_Tk_kuM")
 
-the_notebook = media.Movie("The Notebook",                                                  #Creating an instance the_notebook using the python module media and its class Movie
+#Creating an instance the_notebook using the python module media and its class Movie
+the_notebook = media.Movie("The Notebook",                                                  
                            "Lovestory",
                            "https://upload.wikimedia.org/wikipedia/en/8/86/Posternotebook.jpg",
                            "https://www.youtube.com/watch?v=4M7LIcH8C9U")
-movies = [toy_story, avatar, eat_pray_love, lion, the_blind_side, the_notebook]             #listing out all the movies into a single list
 
-fresh_tomatoes.open_movies_page(movies)                                                     #printing out the movies list
+#listing out all the movies into a single list
+movies = [toy_story, avatar, eat_pray_love, lion, the_blind_side, the_notebook]
+
+#printing out the movies list
+fresh_tomatoes.open_movies_page(movies)                                                     
 
 #print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
+#print(media.Movie.show_trailer.__doc__)
+#print(media.Movie.__init__.__doc__)
 #print(media.Movie.__name__)
 #print(media.Movie.__module__)
                            
--- media.py
+++ media.py
@@ -1,14 +1,27 @@
-import webbrowser                                                                       #importing webbrowser for the youtube trailer of the instances to play
-class Movie():                                                                          #Creating a new class named Movie
+import webbrowser
+
+#Creating a new class
+class Movie():
+    #Doc for the class
     """ This is regarding a movie website. This website displays a                      
         list of my favorite movies and the trailer gets played when
-        the user clicks on the movie poster """                                         #Doc for the class                           
+        the user clicks on the movie poster """                                                                    
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):    #initalizing the class. Whenever the class Movie is used, __init__ gets called
+    #initalizing the class
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        #Doc for the function __init__
+        """ This function __init__ takes as input the title of the movie,
+            the storyline, the poster image and the youtube trailer url
+            and initalizes the variables"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    def show_trailer(self):                                                             #Function to show the trailer when the user clicks on the movie
+        
+    #Function to show the trailer when the user clicks on the movie
+    def show_trailer(self):
+        #Doc for the function show_trailer
+        """ This function show_trailer takes as input the youtube trailer url
+            and shows the trailer when the user clicks the poster of a movie"""
         trailer = webbrowser.open(self.trailer_youtube_url)
